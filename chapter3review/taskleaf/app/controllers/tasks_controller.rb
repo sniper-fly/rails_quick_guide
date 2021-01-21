5d9dd7005cb70bdcfc344f0d98118d338723d2f2
rnakai <rnakai@student.42tokyo.jp>
2021-01-21 14:51:04 +0900

    タスク登録機能を追加

--- chapter3review/taskleaf/app/controllers/tasks_controller.rb
+++ chapter3review/taskleaf/app/controllers/tasks_controller.rb
@@ -6,8 +6,24 @@ class TasksController < ApplicationController
   end
 
   def new
+	@task = Task.new
+  end
+
+  def create
+	task = Task.new(task_params)
+	task.save!
+	redirect_to(tasks_url, notice: "registered task, [#{task.name}]")
+	# flash[:notice] = 'sentence'
+	# redirect_to(tasks_url)これと一緒
   end
 
   def edit
+  end
+
+  private
+
+  def task_params
+	params.require(:task).permit(:name, :description)
+	# パラメータに含まれる"task"キーに対応するバリューを取得し、name, descriptionがあるときだけ取得
   end
 end
