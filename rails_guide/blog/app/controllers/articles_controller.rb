e1cb223e92128840dc2c173e99402b2091979b4f
rnakai <rnakai@student.42tokyo.jp>
2021-01-24 17:14:46 +0900

    一覧表示

--- rails_guide/blog/app/controllers/articles_controller.rb
+++ rails_guide/blog/app/controllers/articles_controller.rb
@@ -1,4 +1,13 @@
 class ArticlesController < ApplicationController
+
+	def index
+		@articles = Article.all
+	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
 	def new
 	end
 
@@ -10,9 +19,6 @@ class ArticlesController < ApplicationController
 		# render plain: params[:article].inspect
 	end
 
-	def show
-		@article = Article.find(params[:id])
-	end
 
 	private
 		def article_params
