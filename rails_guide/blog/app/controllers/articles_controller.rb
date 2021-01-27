7814677908c60a7994fea76a96c3d717c0d1499f
rnakai <rnakai@student.42tokyo.jp>
2021-01-27 12:17:06 +0900

    2番目のモデルの作成

--- rails_guide/blog/app/controllers/articles_controller.rb
+++ rails_guide/blog/app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+	http_basic_authenticate_with name: "rnakai", password: "secret", except: [:index, :show]
+
 	def index
 		@articles = Article.all
 	end
--- rails_guide/blog/app/models/article.rb
+++ rails_guide/blog/app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+	has_many :comments, dependent: :destroy
 	validates :title, presence: true, length: {minimum: 5}
 	# presenceは指定された属性が空でないことを確認する
 end
